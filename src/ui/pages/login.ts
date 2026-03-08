d03ebb009d68e97e3f803bc95d5ac1683e57365f
marisa mourao <mdcmourao@gmail.com>
2026-03-08 18:06:25 +0000

    add LoginPage class and login tests with fixture integration

---
+++ src/ui/pages/login.ts
@@ -0,0 +1,51 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class LoginPage {
+    
+    private page: Page;
+    readonly usernameInput: Locator;
+    readonly passwordInput: Locator;
+    readonly loginButton: Locator;
+    readonly userOptions: Locator;
+    
+    private readonly username = process.env.USER_NAME || '';
+    private readonly password = process.env.PASSWORD || '';
+    
+    constructor(page: Page) {
+        this.page = page;
+        this.usernameInput = page.locator('input[name="username"]');
+        this.passwordInput = page.locator('input[name="password"]');
+        this.loginButton = page.locator('button[type="submit"]');;
+        this.userOptions = page.locator('.oxd-userdropdown-tab');
+    }
+
+    async fillLoginCredencials(username?: string, password?: string) {
+        const user = username !== undefined ? username : this.username;
+        const pass = password !== undefined ? password : this.password;
+
+        await this.usernameInput.fill(user);
+        await this.passwordInput.fill(pass);
+    }
+
+    async redirectToHomePage() {
+        await this.loginButton.click();
+        await this.page.waitForURL('**/dashboard/**', { timeout: 10000 });    
+        await expect(this.page).toHaveURL(/.*dashboard.*/);
+        // await expect(this.page.getByRole('heading')).toContainText('Dashboard');
+    }
+
+    async validateErrorMessage(field: string) {
+        await expect(this.page).toHaveURL(/.*login.*/);
+        const errorMessage = this.page.locator(`.oxd-input-group:has(input[name="${field}"]) span.oxd-input-field-error-message`);
+        await expect(errorMessage).toBeVisible({ timeout: 10000 });
+        // await expect(errorMessage).toContainText(message);
+    }
+
+    async logout() {
+        await this.userOptions.click();
+        const logoutOption = this.page.locator('a[href*="/auth/logout"]');
+        await expect(logoutOption).toBeVisible();
+        await logoutOption.click();
+        await expect(this.page).toHaveURL(/.*login.*/);
+    }
+}
---
+++ src/ui/tests/login.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from "../fixtures/base";
+
+
+test.describe('@B1 @UI Test Automation - Login Tests', () => {
+    test.beforeEach(async ({ page, baseURL }) => {
+        await page.goto(baseURL || '');
+    });
+
+    test('Successful Login', async ({ page, loginPage }) => {
+        // Enter Valid Credentials
+        await loginPage.fillLoginCredencials();
+        // Verify redirection to Dashboard and it loads successfully
+        await loginPage.redirectToHomePage();
+        await expect(page.locator('.oxd-topbar-header-title')).toBeVisible();
+    });
+
+    test('Failed Login - invalid password', async ({ page, loginPage }) => {
+        // Enter an invalid password
+        await loginPage.fillLoginCredencials(process.env.USER_NAME, 'invalidPassword');
+        await loginPage.loginButton.click();
+        await expect(page).toHaveURL(/.*login.*/);
+        await expect(page.getByRole('alert')).toBeVisible({ timeout: 10000 });
+        // await expect(page.getByRole('alert')).toContainText('Invalid credentials');
+    });
+
+    test('Failed Login - empty fields', async ({ loginPage }) => {
+        await loginPage.fillLoginCredencials('', '');
+        await loginPage.loginButton.click();
+        await loginPage.validateErrorMessage('username');
+        await loginPage.validateErrorMessage('password');
+    });
+
+    test('Logout', async ({ loginPage }) => {
+        await loginPage.fillLoginCredencials();
+        await loginPage.redirectToHomePage();
+        await loginPage.logout();
+    });
+});
