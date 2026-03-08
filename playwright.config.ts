1b63fbf0cf4faf944a2da16fa893beb15dda3bb1
marisa mourao <mdcmourao@gmail.com>
2026-03-08 21:10:37 +0000

    stabilize CI/CD by isolating API tests and add docker

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
  */
 export default defineConfig({
   testDir: './src',
+  /* Maximum time one test can run for. */
+  timeout: 90000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,54 +24,58 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright', { outputFolder: 'allure-report' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.API_URL,
-    extraHTTPHeaders: {
-      'x-api-key': process.env.API_KEY || ''
-    },
+    // baseURL: process.env.BASE_URL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    /* Record video only on failure. */
+    video: 'on-first-retry',
+    /* Capture screenshot on failure. */
+    screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'UI-Chromium',
+      testDir: './src/ui/tests',
+      use: { 
+        ...devices['Desktop Chrome'],
+        baseURL: process.env.BASE_URL
+      },
     },
-
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: 'UI-Firefox',
+      testDir: './src/ui/tests',
+      use: { 
+        ...devices['Desktop Firefox'],
+        baseURL: process.env.BASE_URL
+      },
     },
-
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: 'UI-WebKit',
+      testDir: './src/ui/tests',
+      use: { 
+        ...devices['Desktop Safari'],
+        baseURL: process.env.BASE_URL
+      },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'API-Tests',
+      testDir: './src/api/tests',
+      use: { 
+        baseURL: process.env.API_URL,
+        extraHTTPHeaders: {
+          'x-api-key': process.env.API_KEY || ''
+        },
+      },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
